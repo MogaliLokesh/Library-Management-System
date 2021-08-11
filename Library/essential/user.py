97e02d5ac345a8b6ed3a71120eaea9ac59d7aea5
MogaliLokesh <lokeshmogaliofficial@gmail.com>
2021-08-11 19:06:58 +0530

    made changes to popular books

--- Library/essential/user.py
+++ Library/essential/user.py
@@ -4,17 +4,17 @@ from sqlalchemy.orm import Session
 from ..essential import book
 
 #importing get_book method
-get_book=book.get_book
-#method  to create user and update in user table
+get_book = book.get_book
+#method to create user and update in user table
 def create_user(request:schemas.User,db:Session):
-    new_user=models.User(name=request.name,mail=request.mail,books_in_hand=request.books_in_hand)
+    new_user = models.User(name=request.name,mail=request.mail,books_in_hand=request.books_in_hand)
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
 #get user based on the name of the user
 def get_user(name,response:Response,db:Session):
-    user=db.query(models.User).filter(models.User.name==name).first()
+    user = db.query(models.User).filter(models.User.name == name).first()
     if not user:
         raise HTTPException(status_code=404,
                             detail=f'please enter valid user name')
@@ -22,34 +22,42 @@ def get_user(name,response:Response,db:Session):
     return user
 #issues book to an user.update user and inventory
 def issue_book(name:str,book_id:int,response:Response,db:Session):
-    user=get_user(name,response,db)
-    book=get_book(book_id,response,db)
+    user = get_user(name,response,db)
+    book = get_book(book_id,response,db)
     inventory = db.query(models.Inventory).filter(models.Inventory.id == book_id).first()
    
         
     #raise an exception if the book has 0 copies currently in the inventor
-    if inventory.count==0:
+    if inventory.count == 0:
          raise HTTPException(status_code=400,
                             detail=f'no copies of this book are currently available')
-    #raise an exception if a user alredy posses 3 books and still make an isuue request
-    if user.books_in_hand==3:
+    #raise an exception if a user alredy posses 3 books and still make an isuue
+    #request
+    if user.books_in_hand == 3:
         raise HTTPException(status_code=404,
                             detail=f'user already posses maximum number of books')
    
-    inventory.count=inventory.count-1
+    inventory.count = inventory.count - 1
     #print(inventory.id)
-    inventory.total_issues=inventory.total_issues+1
-    user.books_in_hand=user.books_in_hand+1
+    inventory.total_issues = inventory.total_issues + 1
+    user.books_in_hand = user.books_in_hand + 1
     db.commit()
 #rerun book by a user.update user and inventory database
 def return_book(name:str,book_id:int,response:Response,db:Session):
-    user=get_user(name,response,db)
-    book=get_book(book_id,response,db)
+    user = get_user(name,response,db)
+    book = get_book(book_id,response,db)
     inventory = db.query(models.Inventory).filter(models.Inventory.id == book_id).first()
-    #raise an exception if the book  requested by user is not in the inventory
+    #raise an exception if the book requested by user is not in the inventory
     if not book:
         raise HTTPException(status_code=404,
                             detail=f'please enter valid book id')
-    inventory.count=inventory.count+1
-    user.books_in_hand=user.books_in_hand-1
+    inventory.count = inventory.count + 1
+    user.books_in_hand = user.books_in_hand - 1
     db.commit()
+
+
+
+
+def get_all_users(db:Session):
+    users = db.query(models.User).all()
+    return users
--- Library/routers/user.py
+++ Library/routers/user.py
@@ -4,11 +4,9 @@ from sqlalchemy.orm import Session
 from ..essential import user
 
 
-router = APIRouter(
-    prefix = "/user",
-)
-get_db=database.get_db
-#router to create a new user 
+router = APIRouter(prefix = "/user",)
+get_db = database.get_db
+#router to create a new user
 @router.post('/')
 def create_user(request:schemas.User,db:Session=Depends(get_db)):
    
@@ -17,12 +15,18 @@ def create_user(request:schemas.User,db:Session=Depends(get_db)):
 @router.get('/{name}',status_code=200)
 def get_user(name,response:Response,db:Session=Depends(get_db)):
     return user.get_user(name,response,db)
-#router to issue a book to a user.takes book id and name of the user as parameters
+#router to issue a book to a user.takes book id and name of the user as
+#parameters
 @router.put('/{name}/issue_book')
 def issue_book(name,book_id:int,response:Response,db:Session=Depends(get_db),):
     return  user.issue_book(name,book_id,response,db)
 
-#router tp return a book by a user.takes book id and name of the user as parameters
+#router tp return a book by a user.takes book id and name of the user as
+#parameters
 @router.put('/{name}/return_book')
 def return_book(name,book_id:int,response:Response,db:Session=Depends(get_db),):
     return  user.return_book(name,book_id,response,db)
+
+@router.get('/')
+def get_all_users(db:Session=Depends(get_db)):
+    return user.get_all_users(db)
